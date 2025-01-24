dee0f6c4980993b9c6173728e3b03512098f86ae
blakeNaccarato <blake.naccarato@gmail.com>
2025-01-23 17:00:44 -0800

    Add Cappa CLI

--- script.py
+++ script.py
@@ -3,11 +3,27 @@
 # /// script
 # version = "0.0.0"
 # requires-python = "==3.11.*"
+# dependencies = [
+#     "cappa==0.26.4",
+# ]
 # ///
+
+from cappa.base import command, invoke
+
+
+@command
+class Hello:
+    """Say hello."""
+
+    name: str = "you"
+    """Subject of the greeting."""
+
+    def __call__(self):  # noqa: D102
+        print(f"\n\tHello, {self.name}!\n")  # noqa: T201
 
 
 def main():  # noqa: D103
-    print("Hello!")  # noqa: T201
+    invoke(Hello)  # pyright: ignore[reportArgumentType]
 
 
 if __name__ == "__main__":

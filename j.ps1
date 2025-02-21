20db8bb47ae8b072773a27c25f019a7d6e2a524d
blakeNaccarato <blake.naccarato@gmail.com>
2025-02-20 18:18:27 -0800

    Set UV_PREVIEW

--- j.ps1
+++ j.ps1
@@ -1,6 +1,7 @@
 . ./scripts/pre.ps1
 $Env:JUST_COMMAND_COLOR = 'purple'
 $Env:JUST_EXPLAIN = 'true'
-$JustVersion = $Env:JUST_VERSION ? $Env:JUST_VERSION : '1.39.0'
+$Env:UV_PREVIEW = '1'
 $Env:UVX_JUST = "rust-just@$JustVersion"
+$JustVersion = $Env:JUST_VERSION ? $Env:JUST_VERSION : '1.39.0'
 uvx --from "rust-just@$JustVersion" just @Args
